2867576e73a692a580e7b90bb089c184fde87d55
Arvin <arvin.yuwono@binus.ac.id>
2023-03-10 10:48:36 +0700

    Add Checkmark and Cross React Icons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
-
+import {ImCheckmark,ImCross} from 'react-icons/im'
 function Todo({ todo,index,completeTodo,removeTodo}) {
   return (
     <div className="todo"
     style={{textDecoration:todo.isCompleted? "line-through":""}}>
       {todo.text}
       <div>
-        <button onClick={()=>completeTodo(index)}>Complete</button>
-        <button onClick={()=>removeTodo(index)}>x</button>
+        <button onClick={()=>completeTodo(index)}><ImCheckmark/></button>
+        <button onClick={()=>removeTodo(index)}><ImCross/></button>
       </div>
     </div>
   );
